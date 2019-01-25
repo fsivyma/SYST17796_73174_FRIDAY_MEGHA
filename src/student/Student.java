1bbdda26fa301cfc8297703584d5a6a63e38ddea
Ivy <mayanhu@sheridancollege.ca>
2019-01-25 13:40:19 -0500

    Change command

--- src/student/Student.java
+++ src/student/Student.java
@@ -8,9 +8,9 @@
 
 */
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is my first version.
+ * 
+ * 
  */
 
 package student;
